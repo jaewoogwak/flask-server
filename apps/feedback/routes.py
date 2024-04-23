7704c6d030035c131912bb12ce1940c88c95a501
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-04-23 15:42:13 +0900

    test: JSON 응답 양식 테스트
    - 채점 및 피드백에 필요한 정보를 JSON 구조에 맞춰 서버에서 파싱하고 파싱한 내용을 출력

--- apps/feedback/routes.py
+++ apps/feedback/routes.py
@@ -5,5 +5,25 @@ from concurrent.futures import ThreadPoolExecutor
 @main.route('/', methods = ['POST'])
 def feedback():
     params = request.get_json()
-    print(params)
-    return jsonify({"test" : "OK"})
+    
+    # 예시 입력 JSON 파싱
+    if 'FeedBackResults' in params:
+        feedback_results = params['FeedBackResults']
+
+        # 각 문제 정보 출력
+        for question_info in feedback_results:
+            index = question_info['index']
+            question = question_info['question']
+            user_answer = question_info['userAnswer']
+            is_correct = question_info['isCorrect']
+
+            # 파싱된 정보 출력
+            print(f"시험 문제 번호: {index}")
+            print(f"시험 문제: {question}")
+            print(f"사용자 응답: {user_answer}")
+            print(f"정답 여부: {is_correct}")
+            print("---")
+
+        return jsonify({"test": "OK"})
+    
+    return jsonify({"test" : "Error"})
