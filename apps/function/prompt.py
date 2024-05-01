0268cf8085a99d9c63c0a640ba8af22cfb89100b
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-05-01 18:57:53 +0900

    refactor: 문제 생성 프롬프트 수정
    총 문제 수를 정의하는 프롬프트 삭제 및 생성자 수정, 한글 기반 문제로 생성하도록 프롬프트 추가

--- apps/function/prompt.py
+++ apps/function/prompt.py
@@ -1,11 +1,16 @@
 import json
 
 class make_problem_prompt:
+    def __init__(self, text, num_multiple_choice=2, num_short_answer=2):
+        self.input_data = text
+        self.num_multiple_choice = num_multiple_choice
+        self.num_short_answer = num_short_answer
+    
     instruction = """
     당신의 역할은 입력한 데이터를 기반으로 문제를 만들어주는 스터디 멘토입니다. 생성하는 모든 문제는 입력 데이터를 기반으로 검증된 내용이어야 합니다.
     응답은 JSON 형식으로 반환해주세요.
-    문제는 총 {num_questions}문제를 생성하고 문제 구성은 객관식 {num_multiple_choice}문제, 단답형 {num_short_answer}문제로 구성해주세요.
-    각 문제는 case, question, choices, correct_answer, explanation, intent을 포함해야 합니다.
+    문제 구성은 객관식 {num_multiple_choice}문제, 단답형 {num_short_answer}문제로 구성해주세요.
+    각 문제는 case, question, choices, correct_answer, explanation, intent을 포함해야 합니다. 문제 생성은 한글로 해주세요.
     """
 
     context = """
@@ -37,26 +42,10 @@ class make_problem_prompt:
                 "intent": ""                      // string
             },
             {
-                "case": 0,                        // integer
-                "question": "",                   // string
-                "choices": ["1.", "2.", "3.", "4."],      // array of strings
-                "correct_answer": "",             // integer
-                "explanation": "",                // string
-                "intent": ""                      // string
-            },
-            {
                 "case": 1,                        // integer
                 "question": "",                   // string
                 "choices": "빈칸",                  // string
 
-                "correct_answer": "",             // string
-                "explanation": "",                // string
-                "intent": ""                      // string
-            },
-            {
-                "case": 1,                        // integer
-                "question": "",                   // string
-                "choices": "빈칸",                  // string
                 "correct_answer": "",             // string
                 "explanation": "",                // string
                 "intent": ""                      // string
@@ -64,12 +53,6 @@ class make_problem_prompt:
         ]
     }
     """
-
-    def __init__(self, text, num_questions=4, num_multiple_choice=2, num_short_answer=2):
-        self.input_data = text
-        self.num_questions = num_questions
-        self.num_multiple_choice = num_multiple_choice
-        self.num_short_answer = num_short_answer
 
     def set_num_questions(self, num_questions):
         self.num_questions = num_questions
@@ -89,23 +72,28 @@ class make_problem_prompt:
             num_short_answer=self.num_short_answer
         ) + self.context + self.output_template
 
+    def set_custom_prompt(self, user_prompt):
+        self.context += "\n\n문제 생성 중 추가적인 요구 : " + user_prompt
+    
     def get_user_input(self):
         return self.input_data
     
 class marking_problem:
     instruction = """
     너의 역할은 사용자가 풀이한 문제 대한 피드백을 해주는 멘토입니다. 응답은 JSON 형식으로 반환해주세요. 사용자가 풀이한 문제에 대한 정보를 입력으로 주어지면
-    입력에 대한 피드백을 반환하세요. 사용자가 풀이한 문제에 대한 정보는 JSON 형식으로 제공됩니다.
-    피드백의 개수는 입력한 문제 수와 동일합니다. 피드백 내용으로는 index, feedback, iscorrect으로 구성됩니다.
+    입력에 대한 피드백을 반환하세요. 사용자가 풀이한 문제에 대한 정보는 JSON 형식으로 제공됩니다. 입력 JSON의 각 키 값에 대한 설명을 제공합니다.
+    index는 문제 번호 입니다. question은 문제 명입니다. answer는 문제의 답입니다. user_answer는 사용자가 입력한 문제 답 입니다. explanation은 문제 해설입니다. intent는 문제 출제 의도에 대한 내용입니다.
+    응답으로 피드백의 개수는 입력한 문제 수와 동일합니다. 피드백 내용으로는 index, feedback, iscorrect으로 구성됩니다.
     """
     
     context = """
     전체 응답은 output이라는 키를 가진 배열로 구성되어야 합니다.
     index는 문제 번호로 입력 받은 문제의 번호와 동일합니다. 해당 인덱스와 입력된 문제가 매치되어 각 문제에 피드백이 정확히 들어갈 수 있도록 합니다.
-    feedback은 사용자에게 줄 해당 문제에 대한 피드백입니다. 피드백 내용은 문제의 정답과 사용자가 선택한 정답을 비교하여 정답으로 인정될 수 있는지 검토합니다. 
-    문제의 정답과 사용자가 입력한 답이 정답의 범주 내에 있다면 정답으로 간주하도록 합니다. 이 과정은 매우 엄격하게 이뤄져야 하며, 주어진 문제 맥락 및 정보를 파악하여 정답이 맞는지 검증합니다. 
+    feedback은 사용자에게 줄 해당 문제에 대한 피드백입니다. 피드백 내용은 다음 조건을 포함하도록 제시합니다.
+    조건 : 1. 사용자가 선택한 답과
     그리고 검증한 내용을 사용자에게 제시하며 문제의 의도, 문제 해설을 참고하여 사용자에게 다음에도 비슷한 문제를 맞출 수 있도록 조언해주는 내용을 피드백에 추가합니다.
-    iscorrect는 정답 여부로, 사용자가 입력한 답과 문제의 답을 비교하였을 때 정답으로 인정된다면 1, 아니라면 0으로 설정합니다.
+    iscorrect는 정답 여부로 문제의 정답과 사용자가 선택한 정답을 비교하여 정답으로 인정될 수 있는지 검토합니다. 문제의 정답과 사용자가 입력한 답이 정답의 범주 내에 있다면 정답으로 간주하도록 합니다. 
+    이 과정은 매우 엄격하게 이뤄져야 하며, 주어진 문제 맥락 및 정보를 파악하여 정답이 맞는지 검증합니다. 사용자가 입력한 답과 문제의 답을 비교하였을 때 정답으로 인정된다면 1, 아니라면 0으로 설정합니다.
     """
     
     input_data = """"""
