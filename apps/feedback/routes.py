351ff4f96f3b8070651444b9803dbc5134618672
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-05-05 15:17:53 +0900

    Test: json 파싱 테스트
    피드백 라우팅에 입력되는 json 데이터 형식 변화로 파싱 코드 테스트함.
    get_json() 함수는 json을 딕셔너리 형식으로 저장하게 하므로 함수 추가하여 완성할 때 참고

--- apps/feedback/routes.py
+++ apps/feedback/routes.py
@@ -4,26 +4,32 @@ from concurrent.futures import ThreadPoolExecutor
 
 @main.route('/', methods = ['POST'])
 def feedback():
+    # Json을 읽어 딕셔너리로 변환
     params = request.get_json()
     
     # 예시 입력 JSON 파싱
-    if 'FeedBackResults' in params:
-        feedback_results = params['FeedBackResults']
+    if "FeedBackResults" in params:
+        feedback_results = params["FeedBackResults"]
 
         # 각 문제 정보 출력
-        for question_info in feedback_results:
-            index = question_info['index']
-            question = question_info['question']
-            user_answer = question_info['userAnswer']
-            is_correct = question_info['isCorrect']
+        # for info in feedback_results:
+        #     index = info['index']
+        #     question = info['question']
+        #     choices = info['choices']
+        #     correctAnswer = info['correctAnswer']
+        #     user_answer = info['userAnswer']
+        #     isCorrect = info['isCorrect']
+        #     explanation = info['explanation']
+        #     intent = info['intent']
+            
 
-            # 파싱된 정보 출력
-            print(f"시험 문제 번호: {index}")
-            print(f"시험 문제: {question}")
-            print(f"사용자 응답: {user_answer}")
-            print(f"정답 여부: {is_correct}")
-            print("---")
+        #     # 파싱된 정보 출력
+        #     print(f"시험 문제 번호: {index}")
+        #     print(f"시험 문제: {question}")
+        #     print(f"사용자 응답: {user_answer}")
+        #     print(f"정답 여부: {isCorrect}")
+        #     print("---")
 
-        return jsonify({"test": "OK"})
+        return jsonify(params)
     
     return jsonify({"test" : "Error"})
