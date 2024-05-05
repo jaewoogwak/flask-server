4b12a1519799fe1d3df62aa6c7599f306e5a2085
kkwkkwk <rlarjsdn0316@gmail.com>
2024-05-05 12:13:48 +0900

    Update image_test.py

--- apps/function/image_test.py
+++ apps/function/image_test.py
@@ -1,9 +1,10 @@
 import base64
-import requests
-from config import KEY
 import io
 import pdf2image
 import concurrent.futures
+from openai import OpenAI
+
+client = OpenAI()
 
 def convert_pdf_to_images(pdf_content):
     """
@@ -27,28 +28,24 @@ def send_image_to_openai(image, is_PDF=False):
     else:
         image_content = image
     base64_image = encode_image(image_content)
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {KEY}"
-    }
-    payload = {
-        "model": "gpt-4-turbo",
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": "이 이미지에 어떤 내용들이 들어 있는지 한국말로 설명해줘. 이미지에 표나 그래프, 다이어그램이 있다면 절대로 표나 그래프, 다이어그램에 있는 수치나 단어를 언급해서는 안돼. 반드시 수치나 단어가 들어가지 않은, 어떤 개념을 다루는 표와 그래프인지만을 설명해야 해."},
-                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
-                ]
-            }
-        ],
-        "max_tokens": 1200
-    }
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-    response_data = response.json()
-    print(response_data)
-    contents = ' '.join([choice['message']['content'] for choice in response_data['choices']])
-    return contents
+    # 메시지 구성
+    message = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "이 이미지에 어떤 내용들이 들어 있는지 한국말로 설명해줘. 이미지에 표나 그래프, 다이어그램이 있다면 절대로 표나 그래프, 다이어그램에 있는 수치나 단어를 언급해서는 안돼. 반드시 수치나 단어가 들어가지 않은, 어떤 개념을 다루는 표와 그래프인지만을 설명해야 해. 응답은 JSON 형식으로 반환해줘."},
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
+            ]
+        }
+    ]
+    # OpenAI API 호출
+    response_json = client.chat.completions.create(
+        model="gpt-4-turbo",  
+        response_format={"type": "json_object"},
+        messages=message,
+        max_tokens=1200
+    )
+    return response_json.choices[0].message.content
 
 def image_to_openai_response(image_content):
     # 하나의 이미지에 대해 OpenAI 요청을 수행하고 결과를 저장합니다.
