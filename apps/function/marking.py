606dd4571123796267dfa4a980a48f64ce0dd4a1
Kyungho Jeon <jkhcoding@naver.com>
2024-05-08 19:49:59 +0900

    객관식 및 주관식 프롬프트에서 예시 제거

--- apps/function/marking.py
+++ apps/function/marking.py
@@ -11,7 +11,8 @@ chat = ChatOpenAI(
             temperature=0.1,
             streaming=True
         )
-             
+
+
 # 1. 문제 생성 멀티스레드 처리
 def feedback_main(thread_count, input_json):
     # 문제 수만큼 응답 생성
@@ -24,26 +25,13 @@ def feedback_main(thread_count, input_json):
 # 2. 문제 객관식, 단답형 분할
 def feedback_split(input_json):
 
-    # 객관식 문제일 경우
-    if "isCorrect" in input_json:
-
-        # 2-0. 객관식 문제가 맞았을 경우 -> 빈 피드백 리턴
-        if input_json["isCorrect"] == "True":
-            feedback = {
-                "index": input_json["index"],
-                "feedback": " ",
-                "isCorrect": 1
-            }
-            return feedback
-
-        # 2-1. 객관식 문제가 틀렸을 경우 -> 피드백 생성
-        else:
-            return feedback_objective(input_json)
-        
-    # 주관식 문제일 경우
-    else:
-        # 2-2. 주관식 채점 및 피드백 생성
+    # 2-1. 단답형 문제 채점
+    if input_json["choices"] == "빈칸":
         return feedback_subjective(input_json)
+        
+    # 2-2. 객관식 문제 채점
+    else:
+        return feedback_objective(input_json)
 
 
 # 2-1. 객관식 문제 피드백 생성
@@ -52,42 +40,34 @@ def feedback_objective(input_json):
 
     prompts = [
         ("intro", PromptTemplate.from_template(feedback.objective_intro)),
-        ("example", PromptTemplate.from_template(feedback.example)),
         ("start", PromptTemplate.from_template(feedback.start))
     ]
 
     full_prompt = PipelinePromptTemplate(
-        final_prompt=PromptTemplate.from_template(feedback.objective_final),
+        final_prompt=PromptTemplate.from_template(feedback.final),
         pipeline_prompts=prompts
     )
 
     chain = full_prompt | chat
 
     result = chain.invoke({
-        "example_question1": feedback.example_question1,
-        "example_answer1": feedback.example_answer1,
-        "example_question2": feedback.example_question2,
-        "example_answer2": feedback.example_answer2,
-        "example_question3": feedback.example_question3,
-        "example_answer3": feedback.example_answer3,
         "question": input_json
     })
 
-    json_result = json.loads(result.content)
-    return json_result
+    return result.content
 
 
 # 2-2. 주관식 문제 채점 및 피드백 작성
 def feedback_subjective(input_json):
     feedback = marking_problem()
-    
+
     prompts = [
         ("intro", PromptTemplate.from_template(feedback.subjective_intro)),
         ("start", PromptTemplate.from_template(feedback.start))
     ]
 
     full_prompt = PipelinePromptTemplate(
-        final_prompt=PromptTemplate.from_template(feedback.subjective_final),
+        final_prompt=PromptTemplate.from_template(feedback.final),
         pipeline_prompts=prompts
     )
 
@@ -95,8 +75,6 @@ def feedback_subjective(input_json):
 
     result = chain.invoke({
         "question": input_json
-        }
-    )
+    })
 
-    json_result = json.loads(result.content)
-    return json_result
+    return result.content
