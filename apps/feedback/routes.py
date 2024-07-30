b2b449d563eada75e5db919d822f6f2791ad4047
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-07-30 10:09:47 +0900

    docs: apps/feedback/routes.py 주석 추가
    라우팅 설명 주석 추가

--- apps/feedback/routes.py
+++ apps/feedback/routes.py
@@ -4,6 +4,39 @@ from ..function.marking import feedback_main
 
 @main.route('/', methods = ['POST'])
 def feedback():
+    """
+    POST /feeadback/
+    입력한 문제의 채점 문제 피드백 내용을 반환
+
+    Request Body:
+        FeedBackResults = [
+            {
+                "case": int,                                # 문제 유형, 객관식일 경우 0, 주관식일 경우 1
+                "question": "string",                       # 문제 명(이름)
+                "choices": [array of string] or "string",   # 문제 선지, 객관식의 경우 4개의 선택지 배열, 주관식의 경우 "빈칸"
+                "correct_answer": int or "string",          # 문제 정답, 객관식의 경우 선지 번호, 주관식의 경우 정답 string
+                "explanation": "string",                    # 정답 설명
+                "intent": "string"                          # 문제 생성 의도
+            },
+            {},
+            ...
+        ]
+    
+    Returns:
+        array of json: 'index', 'isCorrect', 'feedback' key
+        [
+            {
+                "index": int,           # index
+                "isCorrect": int,       # 맞으면 1, 틀리면 0
+                "feedback": "string"    # 문제 채점 후 피드백 내용
+            },
+            {},
+            ...
+        ]
+        
+    Exceptions:
+        - {"test" : "Error"}: 'FeedBackResults' key가 Request Body에 없는 경우
+    """
     # Json을 읽어 딕셔너리로 변환
     params = request.get_json()
     
