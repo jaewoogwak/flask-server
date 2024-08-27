41ecd6fb9001e9965e4d124da4574e78733ea560
kkwkkwk <rlarjsdn0316@gmail.com>
2024-08-27 22:51:19 +0900

    refactor: 주석 추가 및 dead code 제거

--- apps/function/image_test.py
+++ apps/function/image_test.py
@@ -9,61 +9,114 @@ client = OpenAI()
 
 def convert_pdf_to_images(pdf_content):
     """
-    PDF 파일의 내용을 이미지로 변환합니다.
-    :param pdf_content: PDF 파일의 내용 (bytes)
-    :return: 이미지 데이터 목록
+    PDF 파일의 내용을 이미지로 변환하는 함수
+    
+    Args:
+        pdf_content (bytes): 학습자가 업로드한 강의자료
+    Returns:
+        list: 변환된 이미지 데이터 목록
+    Exceptions:
+        pdf2image.exceptions.PDFPageCountError: PDF 페이지 수를 계산하는 데 실패했을 때 발생
+        pdf2image.exceptions.PDFSyntaxError: PDF 구문 오류로 인해 변환이 실패했을 때 발생
     """
+    # PDF를 이미지로 된 list로 변환 후 PDF_to_openai_responses에 return
     images = pdf2image.convert_from_bytes(pdf_content)
     return images
 
 def encode_image(image):
-    """ 이미지를 base64 문자열로 인코딩합니다. """
+    """ 
+    이미지를 base64 문자열로 인코딩하는 함수
+
+    Args:
+        image (bytes): 학습자가 업로드한 강의자료 중 한 페이지
+    Returns:
+        str: Base64로 인코딩된 문자열
+    Exceptions:
+        TypeError: 이미지 데이터가 잘못된 형식일 때 발생
+    """
+    # 이미지를 base64 문자열로 인코딩 후 send_image_to_openai에 return
     return base64.b64encode(image).decode('utf-8')
 
 def send_image_to_openai(image, is_PDF=False, user_option=None):
-    """ OpenAI API를 사용하여 이미지에 대한 설명을 요청하고, 결과의 'content'만 반환합니다. """
+    """ 
+    OpenAI API를 사용하여 이미지에 대한 분석을 요청하고, 결과를 반환하는 함수
+
+    Args:
+        image (bytes): 학습자가 업로드한 강의자료 중 한 페이지
+        is_PDF (bool): PDF에서 변환된 이미지인지 여부
+        user_option (str): 사용자의 요청사항이 들어간 커스텀 프롬프트
+    Returns:
+        str: OpenAI API에서 반환된 이미지 분석 결과
+    Exceptions:
+        OpenAIError: OpenAI API 요청 실패 시 발생
+        IOError: 이미지 데이터를 처리하는 동안 입출력 오류가 발생할 때
+    """
+    # PDF에서 변환된 이미지인지 아닌지에 따라 다르게 처리
     if is_PDF:
         image_content = io.BytesIO()
         image.save(image_content, format='JPEG')
         image_content = image_content.getvalue()
     else:
         image_content = image
+
+    # base64 문자열로 인코딩 수행
     base64_image = encode_image(image_content)
-    # OpenAI API 호출
+
+    # OpenAI API 호출 이후 결과를 json으로 반환받음
     response_json = request_prompt_img_detecting(base64_image, user_option)
-    # print(response_json)
     return response_json['image_detections']
 
 def image_to_openai_response(image_content):
-    # 하나의 이미지에 대해 OpenAI 요청을 수행하고 결과를 저장합니다.
-    result = send_image_to_openai(image_content)
-    
-    # 결과 출력
-    # print(result)
-    
+    """ 
+    하나의 이미지가 업로드된 경우 이미지 분석 함수를 호출하고 결과를 반환하는 함수
+
+    Args:
+        image_content (bytes): 사용자가 업로드한 강의자료
+    Returns:
+        str: OpenAI API에서 반환된 이미지 분석 결과
+    Exceptions:
+        OpenAIError: OpenAI API 요청 실패 시 발생
+    """
+    # 이미지 분석 함수 호출 후 결과를 반환받음
+    result = send_image_to_openai(image_content)    
     return result
 
 def images_to_openai_responses(images_content):
-    # 각 이미지에 대해 OpenAI 요청을 수행하고 결과를 저장합니다.
+    """ 
+    여러 이미지가 업로드된 경우 이미지 분석 함수를 호출하고 결과를 반환하는 함수
+
+    Args:
+        images_content (list of bytes): 사용자가 업로드한 강의자료
+    Returns:
+        str: 모든 이미지에 대한 분석 결과가 결합된 문자열
+    Exceptions:
+        OpenAIError: OpenAI API 요청 실패 시 발생
+        IOError: 이미지 데이터를 처리하는 동안 입출력 오류가 발생할 때
+    """
+    # 속도 증가를 위한 병렬 처리, 이미지 분석 함수 호출 후 결과를 입력받음
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = list(executor.map(send_image_to_openai, images_content))
-
-    # 결과 출력
-    # for result in results:
-    #     print(result)   
-    
+    # list의 내용을 하나의 문자열로 결합해 반환
     return " ".join(map(str, results))
 
 def PDF_to_openai_responses(pdf_content, user_option=None):
-    # PDF를 이미지로 변환
+    """ 
+    PDF 파일을 이미지로 변환하고, 이미지 분석 함수를 호출하고 결과를 반환하는 함수
+
+    Args:
+        pdf_content (bytes): 사용자가 업로드한 강의자료
+        user_option (str): 사용자의 요청사항이 들어간 커스텀 프롬프트
+    Returns:
+        str: PDF의 각 페이지에 대한 OpenAI API 응답이 결합된 문자열
+    Exceptions:
+        OpenAIError: OpenAI API 요청 실패 시 발생.
+        IOError: 이미지 데이터를 처리하는 동안 입출력 오류가 발생할 때.
+    """
+    # PDF를 이미지 리스트로 변환
     images = convert_pdf_to_images(pdf_content)
 
-    # 각 이미지에 대해 OpenAI 요청을 수행하고 결과를 저장합니다.
+    # 속도 증가를 위한 병렬 처리, 이미지 분석 함수 호출 후 결과를 입력받음
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = list(executor.map(lambda image: send_image_to_openai(image, is_PDF=True, user_option=user_option), images))
-
-    # 결과 출력
-    #for result in results:
-    #    print(result)   
-    
+        results = list(executor.map(lambda image: send_image_to_openai(image, is_PDF=True, user_option=user_option), images)) 
+    # list의 내용을 하나의 문자열로 결합해 반환
     return " ".join(map(str, results))
--- apps/function/langchain.py
+++ apps/function/langchain.py
@@ -62,17 +62,35 @@ def request_prompt(contents, options=None):
     return parsed_response
 
 def request_prompt_img_detecting(contents, options=None):
+    """
+    업로드된 강의자료를 바탕으로 이미지 분석을 요청하는 프롬프트를 생성
+    생성된 프롬프트를 바탕으로 이미지 분석을 요청해 결과를 반환하는 함수
+
+    Args:
+        contents (str): 강의자료를 base64로 인코딩한 문자열
+        options (dict): 사용자 정의 프롬프트 설정을 포함한 옵션
+    Returns:
+        dict: 이미지 분석 결과가 포함된 JSON
+    Exceptions:
+        ValueError: 옵션이 누락된 경우 발생.
+        OpenAIError: OpenAI API 요청 실패 시 발생.
+        JSONDecodeError: JSON 파싱 중 오류가 발생할 때 발생.
+    """
+    # options가 제공되지 않은 경우 오류 메시지 반환
     if options is None:
         return {"error": "Options are missing. Please provide options."}
     
+    # options에서 custom_prompt만 추출
     custom_prompt = options["custom_prompt"]
 
+    # OpenAI의 GPT-4o 모델 인스턴스를 생성하여 설정
     llm = ChatOpenAI(
             model_name="gpt-4o-2024-08-06", 
             temperature=0.2,
             streaming = False,
     )
 
+    # 이미지 분석 프롬프트 생성
     prompt = img_detecting_prompt(contents)
     prompt.set_custom_prompt(custom_prompt)
     system_prompt = prompt.get_system_prompt()
@@ -80,7 +98,7 @@ def request_prompt_img_detecting(contents, options=None):
 
     output_parser = JsonOutputParser()
     
-    # 문제 생성을 위한 프롬프트 설정
+    # 생성한 프롬프트에 강의자료를 base64로 인코딩한 문자열을 추가해 최종 프롬프트 구성
     message = [
         SystemMessage(content=system_prompt),
         HumanMessage(content=[
@@ -88,9 +106,10 @@ def request_prompt_img_detecting(contents, options=None):
         ])
     ]
     
+    # OpenAI에게 최종 프롬프트 전달 후 응답 받기
     response = llm(message)
+    # JSON으로 반환된 분석 결과 중 content(분석 내용)에 관련된 부분만 파싱 후 반환
     parsed_response = output_parser.parse(response.content)
-    
     return parsed_response
 
 # 유저의 입력을 벡터화시키는 함수
