9906d8ed1aeb23be1c9486d7ac690ab322064bb1
kkwkkwk <151744107+kkwkkwk@users.noreply.github.com>
2024-09-12 00:43:34 +0900

    feat: get_uid 추가

--- apps/function/firebase_auth.py
+++ apps/function/firebase_auth.py
@@ -7,7 +7,6 @@ from functools import wraps
 # flask project 시작 시 처음에만 적용
 cred = credentials.Certificate('chat-8d7b2-firebase-adminsdk-xkof8-dc3fc2b5ae.json')
 firebase_admin.initialize_app(cred)
-
 def token_required(f):
     """
     Firebase 인증 토큰이 유효한지 확인하는 데코레이터
@@ -43,4 +42,32 @@ def token_required(f):
 
         # 인증이 성공하면 원래의 라우트 함수를 호출
         return f(*args, **kwargs)
+
     return decorator
+
+def get_uid():
+    """
+    Firebase uid를 가져오기 위한 함수
+
+    Args:
+    Returns:
+        str: user를 구분하기 위한 uid
+    """
+    token = None
+    # Authorization 헤더에서 토큰 파싱
+    if 'Authorization' in request.headers:
+        token = request.headers['Authorization'].split(" ")[1]
+
+    # 토큰이 존재하지 않는 경우, 401 응답을 반환
+    if not token:
+        return jsonify({'message': 'Token is missing!'}), 401
+    try:
+        # Firebase를 사용하여 토큰을 검증
+        decoded_token = firebase_auth.verify_id_token(token)
+        # 사용자 정보를 요청 객체에 첨부
+        request.user = decoded_token
+        uid = decoded_token['uid']
+        return uid
+    except Exception as e:
+        # 토큰이 유효하지 않거나 만료된 경우, 401 응답을 반환
+        return jsonify({'message': 'Token is invalid or expired!'}), 401
