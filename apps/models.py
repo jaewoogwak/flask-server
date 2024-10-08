361ef2219744a21358b294d64b1d1cd8fa1260c4
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-10-08 22:33:27 +0900

    feat: redis client 정의 추가
    model.py에 redis client 설정 통합
    aws로 연결될 주소를 환경 변수로 받아 client 객체 생성

---
+++ apps/models.py
@@ -0,0 +1,17 @@
+import os
+from redis import Redis
+
+# 환경 변수에서 REDIS_ENDPOINT 값 불러오기
+redis_host = os.getenv('REDIS_ENDPOINT')
+
+# Redis 클라이언트 설정 (retriever 저장용)
+redis_client_retriever = Redis(
+    host=redis_host,
+    db=0
+)
+
+# Redis 클라이언트 설정 (학교메일 인증코드 저장용)
+redis_client_auth = Redis(
+    host=redis_host,
+    db=1
+)
