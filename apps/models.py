3c68b8517e81dcf7832089c9d17266e2e6d548c1
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-10-09 08:52:24 +0900

    fix: redis client 객체 선언 수정
    redis client 객체 선언 시 port를 지정하도록 수정

--- apps/models.py
+++ apps/models.py
@@ -7,11 +7,13 @@ redis_host = os.getenv('REDIS_ENDPOINT')
 # Redis 클라이언트 설정 (retriever 저장용)
 redis_client_retriever = Redis(
     host=redis_host,
+    port=6379,
     db=0
 )
 
 # Redis 클라이언트 설정 (학교메일 인증코드 저장용)
 redis_client_auth = Redis(
     host=redis_host,
+    port=6379,
     db=1
 )
